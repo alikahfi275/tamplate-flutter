7aa9ef395a2e649a6820a8a54a3c98a8d60bcb31
ali.lubis <ali.lubis@quadrant-si.id>
2026-03-17 12:11:17 +0700

    Test CI/CD codemagic

--- lib/modules/authentication/screens/auth_screen.dart
+++ lib/modules/authentication/screens/auth_screen.dart
@@ -1,6 +1,5 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 
 import 'package:tamplate_getx/modules/authentication/controllers/auth_controller.dart';
 
